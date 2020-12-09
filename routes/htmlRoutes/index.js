94f477db251be96bfbd46de0cd393174e8b0d7b0
amiedawn <amie.d.carroll@gmail.com>
2020-12-09 15:59:11 -0700

    add html routes

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const router = require('express').Router();
+
+// return the notes.html file from the server
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+// return the index.html file (default) from the server
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
+
 const app = express();
 
-app.listen(3001, () => {
-  console.log('API server now on port 3001!');
+// create a port
+const PORT = process.env.PORT || 3001;
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// use routes
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log('API server now on port ${PORT}!');
 });
